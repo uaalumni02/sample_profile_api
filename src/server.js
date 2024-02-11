ea32fe8e4f275abcac24fd25f403b36981edf255
uaalumni02 <dbell1911@gmail.com>
2024-02-11 11:41:49 -0600

    connect db

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-import mongoose, { connect } from "mongoose";
+import mongoose from "mongoose";
 
 const app = express();
-const { log, error } = console;
+const { log } = console;
 
 const port = process.env.PORT || 3000;
 
@@ -16,12 +16,13 @@ app.use(express.urlencoded({ extended: true }));
 
 const DB_URL = process.env.MONGO_URL;
 
-// mongoose.connect(DB_URL, { useNewUrlParser: true }, (err) => {
-//   if (err) return log("Unable to Connect to MongoDB");
-//   return log("Connection Successful");
-// });
+mongoose.connect(DB_URL)
+  .then(() => {
+    console.log("Connection Successful");
+    app.listen(port, () => log("server is running"));
+  })
+  .catch((error) => {
+    console.error("Unable to Connect to MongoDB:", error);
+  });
 
 app.use("/api", router);
-
-app.listen(port, () => log("server is running"));
-export default app;
