db21d009400683b9fb157f2c180f0ddd41bb3c52
uaalumni02 <dbell1911@gmail.com>
2024-02-24 11:34:34 -0600

    update modal

--- src/models/profile.js
+++ src/models/profile.js
@@ -4,11 +4,31 @@ const { Schema } = mongoose;
 const profileInformationSchema = Schema({
   name: {
     type: String,
-    required: [true, "name is required"]
+    required: [true, "name is required"],
   },
   bio: {
     type: String,
-    required: [true, "bio is required"]
+    required: [true, "bio is required"],
+  },
+  contact_info: {
+    type: String,
+    required: [true, "contact info is required"],
+  },
+  pronouns: {
+    type: String,
+    required: [true, "pronouns are required"],
+  },
+  emergency_contact: {
+    type: String,
+    required: [true, "emergency contacts are required"],
+  },
+  roles: {
+    type: String,
+    required: [true, "roles are required"],
+  },
+  resume: {
+    type: String,
+    required: [true, "resume is required required"],
   },
   __v: {
     type: Number,
