cee9ca249d999d5ef5c8c8a1ae3a9561a26c23fc
uaalumni02 <dbell1911@gmail.com>
2024-02-24 11:47:34 -0600

    update validator

--- src/validator/profile.js
+++ src/validator/profile.js
@@ -3,6 +3,11 @@ const Joi = require("@hapi/joi");
 const schema = Joi.object({
   name: Joi.string().min(3).max(15).required(),
   bio: Joi.string().required(),
+  contact_info: Joi.string().required(),
+  pronouns: Joi.string().required(),
+  emergency_contact: Joi.string().required(),
+  roles: Joi.string().required(),
+  resume: Joi.string().required(),
 });
 
 export default schema;
